cf2a462a861f166c1c20710d4d16cc853a2b9789
Patrick Jimison <patrick.j.jimison@intel.com>
2017-03-26 14:58:34 -0700

    bishop move generation

--- Minimax/Source.cpp
+++ Minimax/Source.cpp
@@ -43,15 +43,15 @@ void PrintBoard(void)
 {
 	using namespace std;
 
-	int a, b;
+	int col, row;
 	string piece;
 
-	for (a=7; a>=0; a--)
+	for (col=7; col>=0; col--)
 	{
-		cout << endl << a + 1 << "  ";
-		for (b=0; b<6; b++)
+		cout << endl << col + 1 << "  ";
+		for (row=0; row<6; row++)
 		{
-			switch(board[a][b])
+			switch(board[col][row])
 			{
 				case 0:
 					piece = "-";
@@ -93,6 +93,88 @@ void PrintBoard(void)
 	cout << endl << endl << "   A B C D E F" << endl << endl;
 }
 
+bool ValidateMove(int colSource, int rowSource, int colDest, int rowDest)
+{
+	using namespace std;
+	
+	cout << "colSource=" << colSource << " rowSource=" << rowSource << " colDest=" << colDest << " rowDest=" << rowDest << endl;
+
+	// check if destination position is the same as source
+	if ((rowSource == rowDest) && (colSource == colDest))
+	{
+		cout << "Illegal: Start and End positions cannot be the same" << endl;
+		return false;
+	}
+	
+
+	bool valid = false;
+	int xDelta = colDest - colSource;
+	int yDelta = rowDest - rowSource;
+
+	switch (board[rowSource][colSource])
+	{
+
+		case 0:
+			cout << "Illegal: There is no game piece in the source location" << endl;
+			break;
+		case bish:
+			cout << "Moving bish" << endl;
+			if(abs(xDelta) == abs(yDelta)) // moving diagonally, legal
+			{
+				if (xDelta > 0 && yDelta > 0) // moving forward-right, legal
+				{
+					if (board[rowSource+1][colSource+1] < 0) // check if the next piece is friendly, illegal
+					{
+						cout << "   Illegal: The next position in your desired move is friendly" << endl;
+						return false;
+					}
+					cout << "   moving bishop forward-right" << endl;
+					/*
+					for (; rowSource<rowDest && colSource<colDest; rowSource++, colSource++)
+					{
+						if (board[rowSource+1][colSource+1] != 0) // encountered another game piece
+						{
+							if (isupper(board[rowSource][colSource])) // opponent piece
+							{
+								// take the piece, move our piece to this position
+							}
+							else // our own piece
+							{
+								// move our piece to previous position
+							}
+						}
+					}
+					*/
+					valid = true;
+				}
+				else if (xDelta < 0 && yDelta > 0) // moving forward-left
+				{
+					if (board[rowSource+1][colSource-1] < 0) // check if the next piece is friendly, illegal
+					{
+						cout << "   Illegal: The next position in your desired move is friendly" << endl;
+						return false;
+					}
+					cout << "   moving bishop forward-left" << endl;
+					valid = true;
+				}
+			}
+			break;
+		case BISH:
+			cout << "Moving BISH" << endl;
+			if (abs(xDelta) == abs(yDelta))
+			{
+				if ((xDelta < 0 && yDelta < 0) || (xDelta > 0 && yDelta < 0)) // moving forward
+				{
+					cout << "   ...moving forward" << endl;
+					valid = true;
+				}
+			}
+			break;
+	}
+
+	return valid;
+}
+
 int main(void)
 {
 	using namespace std;
@@ -119,16 +201,28 @@ int main(void)
 			&& response.substr(2, 1) >= "A" && response.substr(2, 1) <= "F"
 			&& response.substr(3, 1) >= "1" && response.substr(3, 1) <= "8")
 		{
-			int a, b, c, d;
-			a = response[0] - 'A';
-			b = response[1] - '1';
-			c = response[2] - 'A';
-			d = response[3] - '1';
+			int rowSource, colSource, rowDest, colDest;
+			colSource = response[0] - 'A';
+			rowSource = response[1] - '1';
+			colDest = response[2] - 'A';
+			rowDest = response[3] - '1';
 
-			board[d][c] = board[b][a];
-			board[b][a] = 0;
-			PrintBoard();
-
+			if (ValidateMove(colSource, rowSource, colDest, rowDest))
+			{
+				board[rowDest][colDest] = board[rowSource][colSource];
+				board[rowSource][colSource] = 0;
+			}
+			else
+			{
+				cout << "Illegal move detected: " << colSource << rowSource << " -> " << colDest << rowDest << endl;
+			}
+			//board[d][c] = board[b][a];
+			//board[b][a] = 0;
 		}
+		else
+		{
+			cout << endl << "Illegal: the move is outside of the game bounds" << endl << endl;
+		}
+		PrintBoard();
 	}
 }
